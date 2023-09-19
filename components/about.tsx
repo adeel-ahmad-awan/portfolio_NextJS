f3087c0b86a23434edf2ce9fb9c645ce1b7b5a5f
Ad <destontrace@gmail.com>
2023-09-19 17:24:27 +0200

    first commit

---
+++ components/about.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+
+        Upon completing my Bachelor's degree in {" "}
+        <span className="font-medium">Computer Science</span>{" "}, I embarked on my journey into
+        the world of web development with a position at{" "}
+        <span className="font-medium">Coeus Solutions.</span>{" "}
+        The aspect of programming that truly captivates me is the art of problem-solving.
+        There's an indescribable satisfaction that comes with uncovering solutions to intricate challenges.
+      </p>
+      <p>
+        My core skill set encompasses {" "}
+        <span className="font-medium">PHP, JavaScript, React, Next.js, Node.js, and SQL</span>, {" "}
+        with an additional familiarity in the realm of{" "}
+        <span className="font-medium">Machine Learning</span>.{" "}
+        I possess an insatiable curiosity for emerging technologies and am committed to continuous learning.
+        Currently, I am <span className="font-medium">actively seeking a</span>{" "}
+        <span className="font-bold">full-time opportunity as a software developer</span>{" "},
+        eager to contribute my skills and passion to a dynamic team.
+
+      </p>
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games, watching movies. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>.
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,91 @@
+import React from "react";
+import { LuCode } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  // {
+  //   name: "Projects",
+  //   hash: "#projects",
+  // },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Frontend Developer",
+    location: "Muslim Kids TV",
+    description:
+      "I have been a remote frontend developer at Muslim Kids TV for over a year, where I focus on enhancing development efficiency through the creation of reusable UI components in React, HTML, and CSS. I also facilitate collaboration with UX and back-end teams to ensure user-centric integration.",
+    icon: React.createElement(LuCode),
+    date: "2022 - Present",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Coeus Solutions",
+    description:
+    "I have experience as a full-stack developer at Coeus Solutions, where I've been involved in multiple projects, including leading one, and I've also worked on e-commerce plugins.",
+    icon: React.createElement(LuCode),
+    date: "2018 - 2021",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "CorpComment",
+    description:
+      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    imageUrl: corpcommentImg,
+  },
+  {
+    title: "rmtDev",
+    description:
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: rmtdevImg,
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: wordanalyticsImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Node.js",
+  "Git",
+  "PHP",
+  "SQL",
+  "ORM",
+  "PostgreSQL",
+  "Python",
+  "Django",
+] as const;
